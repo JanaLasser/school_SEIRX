f924c428964281c9427e93673052237b82cdb27a
Jana <jana.lasser@ds.mpg.de>
2021-09-08 08:25:32 +0200

    new and cleaner design

--- code/sensitivity_analysis/run_data_creation_ventilation_efficiency.py
+++ code/sensitivity_analysis/run_data_creation_ventilation_efficiency.py
@@ -12,43 +12,20 @@ from multiprocess import Pool
 import psutil
 from tqdm import tqdm
 import sys
-import socket
+
 
 
 ## command line parameters
-# school type for which the script us run. The final optimization combines
-# results from all six school types. We split ensemble runs into the different
-# school types to use all available computational resources.
+# school type for which the script us run.
 st = sys.argv[1]
 school_types = [st]
-
 # number of simulation runs in each ensemble
 N_runs = int(sys.argv[2])
-
-
-minimum_parameters = False
-test = False
-try:
-    mod = sys.argv[3]
-    # is this a test run?
-    if mod == 'test':
-        test = True
-        N_runs = 1
-    # do we just want to create the minimum necessary simulations?
-    elif mod == 'min':
-        minimum_parameters = True
-    else:
-        print('unknown command line parameter {}'.format(test))
-except IndexError:
-    test = False
-
     
 ## I/O
-# source of the contact networks for the calibration runs. There is a randomly
-# generated contact network for each run in the ensemble.
+# source of the contact networks for the calibration runs. 
 contact_network_src = '../../../data/contact_networks/representative_schools'
-# destination of the data for the overall statistics generated in the 
-# calibration run
+# destination of the generated data
 dst = '../../data/sensitivity_analysis/simulation_results/ventilation_efficiency'  
     
 
@@ -72,44 +49,10 @@ params = [(N_runs, st,
            row['ventilation_modification']) \
            for st in school_types \
            for i, row in screening_params.iterrows()]
-
-
-if test:
-    params = params[0:10]
-    print('This is a testrun, scanning only {} parameters with {} runs each.'\
-          .format(len(params), N_runs))
-elif minimum_parameters:
-    params = params[0:15]
-    print('Running the minimum number of necessary simulations ({})'\
-          .format(len(params)))
-else:
-    print('There are {} parameter combinations to sample with {} runs each.'\
-      .format(len(params), N_runs))
     
 
 ## simulation runs
-def run_ventilation_efficiency(params):
-    '''
-    Runs an ensemble of simulations and collects observable statistics. To be 
-    run in parallel on many workers. Note: I/O paths and the number of runs per 
-    ensemble hare hard coded here, because I only want to pass the parameter 
-    values that are being screened in the simulation run to the function via the
-    parallel processing interface.
-    
-    Parameters:
-    -----------
-    param_list : iterable
-        Iterable that contains the values for the parameters N_runs, 
-        school_type, index_case, s_screen_interval, t_screen_interval, s_mask,
-        t_mask, class_size_reduction and ventilation_modification. 
-        These parameters are passed to the simulation.
-        
-    Returns:
-    --------
-    row : dictionary
-        Dictionary of the ensemble statistics of the observables.
-    '''    
-
+def run_ventilation_efficiency(params): 
     # extract the simulation parameters from the parameter list
     N_runs, school_type, index_case, s_screen_interval, t_screen_interval, \
         student_mask, teacher_mask, class_size_reduction, ventilation_mod, \
@@ -132,24 +75,12 @@ def run_ventilation_efficiency(params):
     return ensmbl_results
 
 
-
-# figure out which host we are running on and determine number of cores to
-# use for the parallel programming
-hostname = socket.gethostname()
-if hostname == 'desiato':
-    number_of_cores = 200 # desiato
-    print('running on {}, using {} cores'.format(hostname, number_of_cores))
-elif hostname == 'T14s':
-    number_of_cores = 14 # laptop
-    print('running on {}, using {} cores'.format(hostname, number_of_cores))
-elif hostname == 'marvin':
-    number_of_cores = 28 # marvin
-    print('running on {}, using {} cores'.format(hostname, number_of_cores))
-else:
-    print('unknown host')
-
+# check whether we are running in test or minimal mode and reduce parameter
+# list and N_runs accordingly
+params, N_runs = dcf.check_simulation_mode(params, N_runs)
     
 # run the simulation in parallel on the available cores
+number_of_cores = dcf.get_number_of_cores()
 pool = Pool(number_of_cores)
 
 
@@ -160,7 +91,8 @@ for ensmbl_results in tqdm(pool.imap_unordered(func=run_ventilation_efficiency,
 
 # turn off your parallel workers 
 pool.close()
-    
+
+# sort and save results
 results = results.reset_index(drop=True)
 index_cols = ['school_type', 'index_case',
               'student_screen_interval', 'teacher_screen_interval',

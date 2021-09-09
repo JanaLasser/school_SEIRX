6354d5f7700002d46eae3d4678f4bb08236fe908
Jana <jana.lasser@ds.mpg.de>
2021-09-09 11:46:22 +0200

    fixed typo

---
+++ code/sensitivity_analysis/rerun_last_1.sh
@@ -0,0 +1,34 @@
+python run_data_creation_ventilation_efficiency.py primary 500 last
+python run_data_creation_ventilation_efficiency.py primary_dc 500 last
+python run_data_creation_ventilation_efficiency.py lower_secondary 500 last
+python run_data_creation_ventilation_efficiency.py lower_secondary_dc 500 last
+python run_data_creation_ventilation_efficiency.py upper_secondary 500 last
+python run_data_creation_ventilation_efficiency.py secondary 500 last
+
+python run_data_creation_testing_rate.py primary 500 last
+python run_data_creation_testing_rate.py primary_dc 500 last
+python run_data_creation_testing_rate.py lower_secondary 500 last
+python run_data_creation_testing_rate.py lower_secondary_dc 500 last
+python run_data_creation_testing_rate.py upper_secondary 500 last
+python run_data_creation_testing_rate.py secondary 500 last
+
+python run_data_creation_mask_efficiency.py primary 500 last
+python run_data_creation_mask_efficiency.py primary_dc 500 last
+python run_data_creation_mask_efficiency.py lower_secondary 500 last
+python run_data_creation_mask_efficiency.py lower_secondary_dc 500 last
+python run_data_creation_mask_efficiency.py upper_secondary 500 last
+python run_datac_reation_mask_efficiency.py secondary 500 last
+
+python run_data_creation_test_sensitivity.py primary 500 last
+python run_data_creation_test_sensitivity.py primary_dc 500 last
+python run_data_creation_test_sensitivity.py lower_secondary 500 last
+python run_data_creation_test_sensitivity.py lower_secondary_dc 500 last
+python run_data_creation_test_sensitivity.py upper_secondary 500 last
+python run_data_creation_test_sensitivity.py secondary 500 last
+
+python run_data_creation_reduced_class_size.py primary 500 last
+python run_data_creation_reduced_class_size.py primary_dc 500 last
+python run_data_creation_reduced_class_size.py lower_secondary 500 last
+python run_data_creation_reduced_class_size.py lower_secondary_dc 500 last
+python run_data_creation_reduced_class_size.py upper_secondary 500 last
+python run_data_creation_reduced_class_size.py secondary 500 last

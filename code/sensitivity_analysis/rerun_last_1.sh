d3944c5ef0495d164a2388ba44a3f67b942f901d
Jana <jana.lasser@ds.mpg.de>
2021-09-09 12:00:44 +0200

    adapted to desiato

--- code/sensitivity_analysis/rerun_last_1.sh
+++ code/sensitivity_analysis/rerun_last_1.sh
@@ -1,63 +1,49 @@
-python run_data_creation_ventilation_efficiency.py primary 500 last &
-python run_data_creation_ventilation_efficiency.py primary_dc 500 last &
-python run_data_creation_ventilation_efficiency.py lower_secondary 500 last &
-python run_data_creation_ventilation_efficiency.py lower_secondary_dc 500 last &
-python run_data_creation_ventilation_efficiency.py upper_secondary 500 last &
-python run_data_creation_ventilation_efficiency.py secondary 500 last &
+python3 run_data_creation_ventilation_efficiency.py primary 500 last &
+python3 run_data_creation_ventilation_efficiency.py primary_dc 500 last &
+python3 run_data_creation_ventilation_efficiency.py lower_secondary 500 last &
+python3 run_data_creation_ventilation_efficiency.py lower_secondary_dc 500 last &
+python3 run_data_creation_ventilation_efficiency.py upper_secondary 500 last &
+python3 run_data_creation_ventilation_efficiency.py secondary 500 last &
 
-python run_data_creation_testing_rate.py primary 500 last
-python run_data_creation_testing_rate.py primary_dc 500 last &
-python run_data_creation_testing_rate.py lower_secondary 500 last &
-python run_data_creation_testing_rate.py lower_secondary_dc 500 last &
-python run_data_creation_testing_rate.py upper_secondary 500 last &
-python run_data_creation_testing_rate.py secondary 500 last &
+python3 run_data_creation_testing_rate.py primary 500 last
+python3 run_data_creation_testing_rate.py primary_dc 500 last &
+python3 run_data_creation_testing_rate.py lower_secondary 500 last &
+python3 run_data_creation_testing_rate.py lower_secondary_dc 500 last &
+python3 run_data_creation_testing_rate.py upper_secondary 500 last &
+python3 run_data_creation_testing_rate.py secondary 500 last &
 
-python run_data_creation_mask_efficiency.py primary 500 last &
-python run_data_creation_mask_efficiency.py primary_dc 500 last &
-python run_data_creation_mask_efficiency.py lower_secondary 500 last &
-python run_data_creation_mask_efficiency.py lower_secondary_dc 500 last &
-python run_data_creation_mask_efficiency.py upper_secondary 500 last &
-python run_data_creation_mask_efficiency.py secondary 500 last &
+python3 run_data_creation_mask_efficiency.py primary 500 last &
+python3 run_data_creation_mask_efficiency.py primary_dc 500 last &
+python3 run_data_creation_mask_efficiency.py lower_secondary 500 last &
+python3 run_data_creation_mask_efficiency.py lower_secondary_dc 500 last &
+python3 run_data_creation_mask_efficiency.py upper_secondary 500 last &
+python3 run_data_creation_mask_efficiency.py secondary 500 last &
 
-python run_data_creation_test_sensitivity.py primary 500 last &
-python run_data_creation_test_sensitivity.py primary_dc 500 last &
-python run_data_creation_test_sensitivity.py lower_secondary 500 last &
-python run_data_creation_test_sensitivity.py lower_secondary_dc 500 last &
-python run_data_creation_test_sensitivity.py upper_secondary 500 last &
-python run_data_creation_test_sensitivity.py secondary 500 last &
+python3 run_data_creation_test_sensitivity.py primary 500 last &
+python3 run_data_creation_test_sensitivity.py primary_dc 500 last &
+python3 run_data_creation_test_sensitivity.py lower_secondary 500 last &
+python3 run_data_creation_test_sensitivity.py lower_secondary_dc 500 last &
+python3 run_data_creation_test_sensitivity.py upper_secondary 500 last &
+python3 run_data_creation_test_sensitivity.py secondary 500 last &
 
-python run_data_creation_reduced_class_size.py primary 500 last &
-python run_data_creation_reduced_class_size.py primary_dc 500 last &
-python run_data_creation_reduced_class_size.py lower_secondary 500 last &
-python run_data_creation_reduced_class_size.py lower_secondary_dc 500 last &
-python run_data_creation_reduced_class_size.py upper_secondary 500 last &
-python run_data_creation_reduced_class_size.py secondary 500 last &
+python3 run_data_creation_reduced_class_size.py primary 500 last &
+python3 run_data_creation_reduced_class_size.py primary_dc 500 last &
+python3 run_data_creation_reduced_class_size.py lower_secondary 500 last &
+python3 run_data_creation_reduced_class_size.py lower_secondary_dc 500 last &
+python3 run_data_creation_reduced_class_size.py upper_secondary 500 last &
+python3 run_data_creation_reduced_class_size.py secondary 500 last &
 
-python run_data_creation_added_friendship_cotacts.py primary 500 last &
-python run_data_creation_added_friendship_cotacts.py primary_dc 500 last &
-python run_data_creation_added_friendship_cotacts.py lower_secondary 500 last &
-python run_data_creation_added_friendship_cotacts.py lower_secondary_dc 500 last &
-python run_data_creation_added_friendship_cotacts.py upper_secondary 500 last &
+python3 run_data_creation_added_friendship_cotacts.py primary 500 last &
+python3 run_data_creation_added_friendship_cotacts.py primary_dc 500 last &
+python3 run_data_creation_added_friendship_cotacts.py lower_secondary 500 last &
+python3 run_data_creation_added_friendship_cotacts.py lower_secondary_dc 500 last &
+python3 run_data_creation_added_friendship_cotacts.py upper_secondary 500 last &
 
-python run_data_creation_transmissibility.py primary 500 last &
-python run_data_creation_transmissibility.py primary_dc 500 last &
-python run_data_creation_transmissibility.py lower_secondary 500 last &
-python run_data_creation_transmissibility.py lower_secondary_dc 500 last &
-python run_data_creation_transmissibility.py upper_secondary 500 last &
-python run_data_creation_transmissibility.py secondary 500 last &
-
-python run_data_creation_worst_case.py primary 500 last &
-python run_data_creation_worst_case.py primary_dc 500 last &
-python run_data_creation_worst_case.py lower_secondary 500 last &
-python run_data_creation_worst_case.py lower_secondary_dc 500 last &
-python run_data_creation_worst_case.py upper_secondary 500 last &
-python run_data_creation_worst_case.py secondary 500 last &
-
-python run_data_creation_worst_case_and_vaccinations.py primary 500 last &
-python run_data_creation_worst_case_and_vaccinations.py primary_dc 500 last &
-python run_data_creation_worst_case_and_vaccinations.py lower_secondary 500 last &
-python run_data_creation_worst_case_and_vaccinations.py lower_secondary_dc 500 last &
-python run_data_creation_worst_case_and_vaccinations.py upper_secondary 500 last &
-python run_data_creation_worst_case_and_vaccinations.py secondary 500 last &
+python3 run_data_creation_transmissibility.py primary 500 last &
+python3 run_data_creation_transmissibility.py primary_dc 500 last &
+python3 run_data_creation_transmissibility.py lower_secondary 500 last &
+python3 run_data_creation_transmissibility.py lower_secondary_dc 500 last &
+python3 run_data_creation_transmissibility.py upper_secondary 500 last &
+python3 run_data_creation_transmissibility.py secondary 500 last &
 
 

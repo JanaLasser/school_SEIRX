f70c7b2f4d6f27819c56cdd53553b3221a728a23
Jana <jana.lasser@ds.mpg.de>
2021-09-09 11:58:45 +0200

    added the rest of the sensitivity analyses

--- code/sensitivity_analysis/rerun_last_1.sh
+++ code/sensitivity_analysis/rerun_last_1.sh
@@ -5,7 +5,7 @@ python run_data_creation_ventilation_efficiency.py lower_secondary_dc 500 last &
 python run_data_creation_ventilation_efficiency.py upper_secondary 500 last &
 python run_data_creation_ventilation_efficiency.py secondary 500 last &
 
-python run_data_creation_testing_rate.py primary 500 last &
+python run_data_creation_testing_rate.py primary 500 last
 python run_data_creation_testing_rate.py primary_dc 500 last &
 python run_data_creation_testing_rate.py lower_secondary 500 last &
 python run_data_creation_testing_rate.py lower_secondary_dc 500 last &
@@ -32,3 +32,32 @@ python run_data_creation_reduced_class_size.py lower_secondary 500 last &
 python run_data_creation_reduced_class_size.py lower_secondary_dc 500 last &
 python run_data_creation_reduced_class_size.py upper_secondary 500 last &
 python run_data_creation_reduced_class_size.py secondary 500 last &
+
+python run_data_creation_added_friendship_cotacts.py primary 500 last &
+python run_data_creation_added_friendship_cotacts.py primary_dc 500 last &
+python run_data_creation_added_friendship_cotacts.py lower_secondary 500 last &
+python run_data_creation_added_friendship_cotacts.py lower_secondary_dc 500 last &
+python run_data_creation_added_friendship_cotacts.py upper_secondary 500 last &
+
+python run_data_creation_transmissibility.py primary 500 last &
+python run_data_creation_transmissibility.py primary_dc 500 last &
+python run_data_creation_transmissibility.py lower_secondary 500 last &
+python run_data_creation_transmissibility.py lower_secondary_dc 500 last &
+python run_data_creation_transmissibility.py upper_secondary 500 last &
+python run_data_creation_transmissibility.py secondary 500 last &
+
+python run_data_creation_worst_case.py primary 500 last &
+python run_data_creation_worst_case.py primary_dc 500 last &
+python run_data_creation_worst_case.py lower_secondary 500 last &
+python run_data_creation_worst_case.py lower_secondary_dc 500 last &
+python run_data_creation_worst_case.py upper_secondary 500 last &
+python run_data_creation_worst_case.py secondary 500 last &
+
+python run_data_creation_worst_case_and_vaccinations.py primary 500 last &
+python run_data_creation_worst_case_and_vaccinations.py primary_dc 500 last &
+python run_data_creation_worst_case_and_vaccinations.py lower_secondary 500 last &
+python run_data_creation_worst_case_and_vaccinations.py lower_secondary_dc 500 last &
+python run_data_creation_worst_case_and_vaccinations.py upper_secondary 500 last &
+python run_data_creation_worst_case_and_vaccinations.py secondary 500 last &
+
+
